fbe0e4691f1c59aa7d17f7dc4596a5720f936d3c
ClaudioFonseca <claudioandre-17@hotmail.com>
2022-06-15 16:06:01 +0100

    adicionada a função de mover e parar o smile

--- index.php
+++ index.php
@@ -30,11 +30,29 @@
         <button type="button" id="botao" class="btn btn-primary" onclick="myFunction()">Click Me!</button>
 
 
+        <!-- Imagem com funções mouse over e mouse out -->
+        <img id="smile" onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="smiley.gif" alt="Smiley" width="32" height="32">
+
+        <p><p>
+        <!-- Botão para mover para a direita -->
+        <input type = "button" value = "Start" onclick = "moveRight();" />
+
+        <!-- Botão para a parar -->
+        <input type = "button" value = "Stop" onclick = "stop();" />
+
+
+            <!-- Video
+            <video width="320" height="240" controls>
+                <source src="movie.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>-->
+
     </div>
 
 
-    <!-- Função para mudar a cor ao clicar -->
+
     <script>
+        <!-- Função para mudar a cor ao clicar -->
         function myFunction() {
 
             <!-- ALTERAR O TEXTO DO BOTAO -->
@@ -43,6 +61,48 @@
             <!-- ALTERAR A COR DO BOTAO -->
             document.getElementById("botao").style.background='#00FFFF';
         }
+
+
+        <!-- Função para aumentar imagem -->
+        function bigImg(x) {
+            x.style.height = "100px";
+            x.style.width = "100px";
+        }
+
+        <!-- Função para por imagem normal -->
+        function normalImg(x) {
+            x.style.height = "32px";
+            x.style.width = "32px";
+        }
+
+
+
+        var imgObj = null;
+        var animate ;
+
+        <!-- Função para inicializar a imagem -->
+        function init() {
+            imgObj = document.getElementById('smile');
+            imgObj.style.position= 'relative';
+            imgObj.style.left = '0px';
+        }
+
+
+        <!-- Função para mover a imagem para a direita -->
+        function moveRight() {
+            imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+            animate = setTimeout(moveRight,20);    // call moveRight in 20msec
+        }
+
+        <!-- Função para parar -->
+        function stop() {
+            clearTimeout(animate);
+            imgObj.style.left = '0px';
+        }
+
+        window.onload = init;
+
+
     </script>
 
 
